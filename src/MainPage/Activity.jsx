2b4634a1d9937e00583ddfedf3739044b65d6f02
prem-ramamoorthy <pkrprem2005@gmail.com>
2025-09-23 12:53:52 +0530

    Alignment of Activity

--- src/MainPage/Activity.jsx
+++ src/MainPage/Activity.jsx
@@ -16,23 +16,39 @@ function Activity() {
   ].map((props, index) => <ActivityP key={index} {...props} />);
 
   return (
-    <div className="div2 bg-white rounded-lg sm:rounded-xl lg:rounded-2xl
-                    [grid-area:10/1/15/2] 
-                    sm:[grid-area:5/1/8/3] 
-                    md:[grid-area:5/1/9/4] 
-                    lg:[grid-area:5/1/9/5] 
-                    xl:[grid-area:5/1/10/5]"
+    <div
+      className="
+                div2 rounded-lg sm:rounded-xl lg:rounded-2xl
+                [grid-area:10/1/15/2]
+                sm:[grid-area:5/1/8/3]
+                md:[grid-area:5/1/9/4]
+                lg:[grid-area:5/1/9/5]
+                xl:[grid-area:5/1/10/5]
+                bg-white
+                max-w-full
+                min-h-0
+                overflow-hidden
+                flex flex-col
+                "
     >
-      <div className="header flex flex-row justify-between align-middle m-2">
-        <h2 className="text-[18px] font-[600] text-black m-4">Activity</h2>
-        <div className="buttonContainer flex gap-2 m-2 flex-row">
-          {Buttons}
-        </div>
+      <div className="header flex items-center justify-between gap-2 px-4 py-3 shrink-0">
+        <h2 className="text-[18px] font-[600] text-black m-0">Activity</h2>
+        <div className="buttonContainer flex gap-2">{Buttons}</div>
       </div>
-      <div className="container -mt-3 md:-mt-4 lg:mt-0 xl:mt-0">
+
+      <div
+        className="
+      container
+      max-w-full
+      min-h-0
+      flex-1
+      overflow-auto
+    "
+      >
         {ActivityPs}
       </div>
     </div>
+
   )
 }
 
