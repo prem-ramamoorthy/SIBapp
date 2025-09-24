5c14d6ff2a20098d930ce8d080be930dd4a7cb10
prem-ramamoorthy <pkrprem2005@gmail.com>
2025-09-24 20:14:01 +0530

    Activity field completed

--- src/MyActivity/Components/Activity.jsx
+++ src/MyActivity/Components/Activity.jsx
@@ -5,26 +5,35 @@ function Activity(
 ) {
     if (header) {
         return (
-            <div className="heade flex flex-row justify-around border-b-1 pt-4 px-2  bg-gray-200 rounded-t-2xl overflow-x-scroll items-center">
-                <p className="min-w-[120px] font-bold text-center">Date</p>
-                <p className="min-w-[120px] font-bold text-center">Type</p>
-                <p className="min-w-[120px] font-bold text-center"> Direction</p>
-                <p className="min-w-[120px] font-bold text-center">Member Name</p>
-                <p className="min-w-[120px] font-bold text-center">Details</p>
-                <p className="min-w-[120px] font-bold text-center">Status</p>
-                <p className="min-w-[120px] font-bold text-center">Action</p>
+            <div className="heade flex flex-row justify-around border-b-1 pt-4 px-2 pb-3  bg-gray-200 rounded-t-2xl w-max items-center lg:w-full xl:w-full ">
+                <p className="w-[120px] font-bold  mx-2 text-center">Date</p>
+                <p className="w-[120px] font-bold  mx-2 text-center">Type</p>
+                <p className="w-[120px] font-bold  mx-2 text-center"> Direction</p>
+                <p className="w-[120px] font-bold   mx-2 text-center">Member Name</p>
+                <p className="w-[120px] font-bold   mx-2 text-center">Details</p>
+                <p className="w-[120px] font-bold   mx-2 text-center">Status</p>
+                <p className="w-[120px] font-bold   mx-2 text-center">Action</p>
             </div>
         )
     }
     return (
-        <div className="activity-field flex flex-row justify-around p-2 px-2 rounded-t-2xl overflow-x-scroll items-center">
-            <p className="min-w-[120px] text-center font-semibold">Date</p>
-            <p className="min-w-[120px] text-center">Type</p>
-            <p className="min-w-[120px] text-center"> Direction</p>
-            <p className="min-w-[120px] text-center font-semibold">Member Name</p>
-            <p className="min-w-[120px] text-center">Details</p>
-            <p className="min-w-[120px] text-center">Status</p>
-            <p className="min-w-[120px] text-center">Action</p>
+        <div className="activity-field flex flex-row justify-around py-2 items-center w-max cursor-auto lg:w-full xl:w-full">
+            <p className="w-[120px] text-center font-semibold  text-nowrap mx-2 overflow-x-hidden">20 sept 2025</p>
+            <button  className="w-[120px] font-semibold cursor-no-drop text-blue-800
+            text-nowrap text-center  mx-2 overflow-x-hidde bg-blue-300/50 rounded-lg p-1 mb-1">
+                TYFCB
+            </button>
+            <p className="w-[120px] text-nowrap  text-center  mx-2 overflow-x-hidden">Given</p>
+            <p className="w-[120px]  text-nowrap text-center mx-2  font-semibold overflow-x-hidden">yonesh Murugan</p>
+            <p className="w-[120px] text-nowrap text-center  mx-2 overflow-x-hide overflow-x-hidden">Hi hello everyone how are you</p>
+            <button  className="w-[120px] font-semibold cursor-no-drop text-green-800
+            text-nowrap text-center  mx-2 overflow-x-hidde bg-green-300/50 rounded-xl p-1 mb-1">
+                Approved
+            </button>
+            <button  className="w-[120px] font-bold cursor-pointer
+            text-nowrap text-center  mx-2 overflow-x-hidden border-amber-500 border-3 rounded-xl p-1 mb-1">
+                View Details
+            </button>
         </div>
     )
 }
--- src/MyActivity/FullActivity.jsx
+++ src/MyActivity/FullActivity.jsx
@@ -2,9 +2,23 @@ import Activity from "./Components/Activity"
 
 function FullActivity() {
   return (
-    <div className="w-[98%] min-h-[330px] m-2 border-2 border-gray-400 bg-white rounded-2xl">
-        <Activity header={true}/>
+    <div className="activity-container w-[98%] min-h-[330px] m-2 border-2 border-gray-400 bg-white rounded-2xl">
+      <div className="overflow-scroll h-[330px]">
+        <Activity header={true} />
         <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+        <Activity />
+      </div>
     </div>
   )
 }
