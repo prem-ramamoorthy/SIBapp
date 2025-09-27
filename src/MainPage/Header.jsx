d3fcc7de847fa9eb9b49529872f1c600bf423861
prem-ramamoorthy <pkrprem2005@gmail.com>
2025-09-27 23:36:31 +0530

    .

--- src/MainPage/Header.jsx
+++ src/MainPage/Header.jsx
@@ -4,13 +4,13 @@ import Sidebar from './SideBar/SideBar'
 
 function Header() {
   return (
-    <div className="div1 bg-white rounded-lg sm:rounded-xl lg:rounded-2xl
+    <div className="sticky top-0 div1 bg-white rounded-lg sm:rounded-xl lg:rounded-2xl
                   [grid-area:1/1/2/2] 
                   sm:[grid-area:1/1/2/5] 
                   md:[grid-area:1/1/2/7] 
                   lg:[grid-area:1/1/2/9] 
                   xl:[grid-area:1/1/2/11]
-                  flex justify-between align-middle"
+                  flex justify-between align-middle z-50"
     >
       <div className="front flex flex-row mt-2">
         <div className="icon mt-4 pl-5"><Sidebar /></div>
