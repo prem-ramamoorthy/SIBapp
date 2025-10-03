44c676e9d7cf78bd0d24d653a5a2b5ce4e9cf42f
prem-ramamoorthy <pkrprem2005@gmail.com>
2025-10-04 01:43:49 +0530

    Forgot password gmail sending system completed

--- src/ResetPassword/ResetPassword.jsx
+++ src/ResetPassword/ResetPassword.jsx
@@ -24,7 +24,7 @@ const ResetPassword = () => {
       if (data.message === "Password reset email sent successfully") {
         setMessage("Password reset link has been generated and sent successfully.");
       } else {
-        console.log(data.message);
+        console.log(data.error);
         setMessage("Unknown error occurred") ;
       }
     } catch (error) {
