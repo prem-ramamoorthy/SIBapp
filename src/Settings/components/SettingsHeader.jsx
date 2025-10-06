2aa41f378936e56ed18e0e25b49a75cbba1c2537
prem-ramamoorthy <pkrprem2005@gmail.com>
2025-10-06 19:06:27 +0530

    completed save button in settings

--- src/Settings/components/SettingsHeader.jsx
+++ src/Settings/components/SettingsHeader.jsx
@@ -1,5 +1,14 @@
+import { NavLink , useNavigate } from "react-router-dom";
 
 export default function SettingsHeader({ title }) {
+
+  const navigate = useNavigate() ;
+
+  const handleclick = ()=>{
+    alert("changes saved Sucessfully !")
+    navigate('/dashboard')
+  }
+
   return (
     <header className="flex items-center justify-between rounded-2xl border border-slate-200 bg-white px-4 py-3 shadow-sm">
       <h1 className="text-xl font-semibold text-slate-900">{title}</h1>
@@ -8,6 +17,7 @@ export default function SettingsHeader({ title }) {
         <button
           type="button"
           className="rounded-md bg-red-600 px-6 py-2 m-2 text-sm font-semibold text-white hover:bg-red-600/70 focus:outline-none focus:ring-2 focus:ring-slate-400"
+          onClick={handleclick}
         >
           Save
         </button>
