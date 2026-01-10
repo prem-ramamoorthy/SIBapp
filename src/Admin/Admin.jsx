d8394bfe726a16f7c2282d96191db8ff983bd4f0
prem-ramamoorthy <pkrprem2005@gmail.com>
2026-01-10 23:26:56 +0530

    .

--- src/Admin/Admin.jsx
+++ src/Admin/Admin.jsx
@@ -1,5 +1,3 @@
-import React, { useState } from 'react'
-import Header from './AdminHeader'
 import Stats from './Components/Stat'
 import StatTable from './Components/StatTable'
 import LineGraph from './Components/LineGraph'
@@ -8,19 +6,18 @@ import RevenueByChapterChart from './Components/Graph'
 export default function Admin() {
   return (
     <div className="min-h-screen w-full  transition-colors duration-300">
-      <div className="fixed top-2 left-0 right-0 z-50  shadow-md">
-        <Header />
-      </div>
-        <div className="pt-20 w-full px-4 sm:px-6 lg:px-8 py-8">
+      <div className="w-full px-4 sm:px-6 lg:px-8 py-8">
         <div className="max-w-7xl mx-auto space-y-6">
-          <h1 className="text-3xl font-bold">Admin Portal</h1>
-          {/* Additional admin components can be added here */}
+          <h1 className="text-3xl font-bold dark:text-white">Admin Portal</h1>
         </div>
         <Stats />
         <StatTable />
+        <div className="-mt-10 max-w-7xl mx-auto space-y-6">
+          <h1 className="text-3xl font-bold dark:text-white">Reports</h1>
+        </div>
         <LineGraph />
         <RevenueByChapterChart />
-        </div>
       </div>
+    </div>
   )
 }
