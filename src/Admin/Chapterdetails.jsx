d88247c5a1427486c8a40b6d78567f1818de34ed
prem-ramamoorthy <pkrprem2005@gmail.com>
2026-01-12 11:33:23 +0530

    chapter performance

--- src/Admin/Chapterdetails.jsx
+++ src/Admin/Chapterdetails.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
 import ChapterStat from './Components/ChapterStat';
 import CreatePresidentForm from './Components/CreatePresident';
 import PresidentRoleManagement from './Components/PresidentRole';
 import MemberDetailes from './Components/Memberdetails';
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
 const ChapterDetails = () => {
-    return (
+    const { id } = useParams();
+    const [chapter, setChapter] = useState(null);
 
+    useEffect(() => {
+        const fetchChapter = async () => {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_BACKEND_SERVER}/chapter/main/getchapterbyid/${id}` , { credentials: 'include' });
+                if (response.ok) {
+                    const data = await response.json();
+                    setChapter(data);
+                }
+            } catch (error) {
+                console.error('Failed to fetch chapter:', error);
+            }
+        };
+        fetchChapter();
+    }, [id]);
+
+    return (
         <div className="w-full px-4 sm:px-6 lg:px-8 py-8">
             <div className="max-w-7xl mx-auto space-y-6">
-                <h1 className="text-3xl font-bold dark:text-white">(Chapter Name)</h1>
+                <h1 className="text-3xl font-bold dark:text-white">
+                    {chapter ? `${chapter.chapter_name} Chapter` : '(Chapter Name)'}
+                </h1>
             </div>
-            <ChapterStat />
+            <ChapterStat chapterid={id}/>
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
                 <div className="w-full">
                     <CreatePresidentForm />
